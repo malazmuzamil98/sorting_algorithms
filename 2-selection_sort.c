bfe18413cbd6570a943adaea13a38e0cae90998c
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-02-19 18:48:00 +0200

    t2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,13 +22,13 @@ void selection_sort(int *array, size_t size)
 			{
 				min_idx = j;
 			}
-			if (min_idx != i)
+		}
+		if (min_idx != i)
 			{
 				temp = array[i];
 				array[i] = array[min_idx];
 				array[min_idx] = temp;
 				print_array(array, size);
 			}
-		}
 	}
 }
