28d1d0da6b7a252f1386336e4eef6f5e5714b248
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-02-19 19:54:18 +0200

    t3

--- sort.h
+++ sort.h
@@ -22,6 +22,9 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 
 
 #endif
